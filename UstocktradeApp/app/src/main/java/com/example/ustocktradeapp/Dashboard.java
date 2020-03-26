f3d283bab26985ad5ecd9c79abf4d2a2a237df6c
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2020-03-26 12:00:07 +0530

    project files added

---
+++ UstocktradeApp/app/src/main/java/com/example/ustocktradeapp/Dashboard.java
@@ -0,0 +1,99 @@
+package com.example.ustocktradeapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Dashboard extends AppCompatActivity {
+
+    ListView listView;
+    String mTitle[] = {"Country Codes","Change Passwords","Dummy","Dummy","Dummy","Dummy","Dummy","Dummy","Dummy"};
+    int images[] = {R.drawable.logo};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        listView.findViewById(R.id.listView);
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0){
+                    Toast.makeText(Dashboard.this,"description",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        int rImgs[];
+
+        MyAdapter(Context c,String title[],int imgs[]){
+            super(c, R.layout.row, R.id.title, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rImgs = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.title);
+
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+
+
+
+            return row;
+        }
+    }
+}
---
+++ UstocktradeApp/app/src/main/java/com/example/ustocktradeapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ustocktradeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    Button btLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btLogin = findViewById(R.id.bt_submit);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etEmail.getText().toString().equals("test@test.com") && etPassword.getText().toString().equals("test123")) {
+                        startActivity(new Intent(MainActivity.this,Dashboard.class));
+                }else {
+                    Toast.makeText(getApplicationContext(),"Invalid Email or Password!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
